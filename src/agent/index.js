22dd43c0d4b388a5492038558608832273be7663
Abhishek Sohagiya <abhishek.s@ghanshyam-s.ah.zymrinc.com>
2020-12-15 12:43:37 +0530

    Remvoed safar view controller support

--- src/agent/index.js
+++ src/agent/index.js
@@ -3,9 +3,6 @@ var WebViewAgent = require('./webview');
 
 module.exports = function getAgent(callback) {
   return BrowserAgent.isAvailable(function (available) {
-    if (available) {
-      return callback(null, new BrowserAgent());
-    }
     return callback(null, new WebViewAgent());
   });
 };
